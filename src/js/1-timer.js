990b8b0b33c782edc6e229057e9cdfa5fbbb4048
Seresun <slavnyunstoppable22@gmail.com>
2024-08-11 11:28:41 +0400

    Initial commit with all files

---
+++ src/js/1-timer.js
@@ -0,0 +1,79 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startButton = document.querySelector('[data-start]');
+const datetimePicker = document.querySelector('#datetime-picker');
+let timerInterval;
+
+startButton.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    if (selectedDate <= new Date()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+flatpickr(datetimePicker, options);
+
+startButton.addEventListener('click', () => {
+  const selectedDate = new Date(datetimePicker.value);
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+  timerInterval = setInterval(() => {
+    const now = new Date();
+    const remainingTime = selectedDate - now;
+
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      updateTimer(0, 0, 0, 0);
+      datetimePicker.disabled = false;
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(remainingTime);
+    updateTimer(days, hours, minutes, seconds);
+  }, 1000);
+});
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer(days, hours, minutes, seconds) {
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent =
+    addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent =
+    addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
